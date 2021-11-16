f978b9c575d426e92f35964095d88369229d5baf
chrdre <christerdr@outlook.com>
2021-11-16 14:45:37 +0100

    Training CNN works

--- generator/generator.py
+++ generator/generator.py
@@ -1,6 +1,7 @@
 import numpy as np
 import subprocess
-from lammps_simulator import Simulator, SlurmGPU, SlurmCPU
+from lammps_simulator import Simulator
+from lammps_simulator.computer import SlurmGPU, SlurmCPU
 from run_torch_model import create_dataloader, RunTorchCNN
 from simplexgrid import SimplexGrid, SeedGenerator
 from pathlib import Path
@@ -8,6 +9,7 @@ from data_analyzer import Dataset
 import os
 import shutil
 import torch
+import sys
 
 
 class Generator:
@@ -34,20 +36,32 @@ class Generator:
     sbatch --dependency=afterok:$(sbatch --parsable job.sh)
     """
 
-    def __init__(self, paths, model):
+    def __init__(self, paths, criterion=None, params=None,
+                 optimizer=None, epochs=2500):
+
+        if not isinstance(criterion, str) and criterion is not None:
+            raise ValueError(
+                'criterion must be of type str specifying which loss functions to use')
+        if not isinstance(optimizer, str) and optimizer is not None:
+            raise ValueError(
+                'optimizer must be of type str specifying which optimizer to use')
         self.job_ids = None
         self.generation = 0
         self.path_potential = Path(paths['potential'])
         self.path_lmp = Path(paths['lammps'])
+        self.path_cnn = Path(paths['cnn'])
         self.generation = 0
-        self.model = model
+        self.criterion = criterion
+        self.params = params
+        self.optimizer = optimizer
+        self.epochs = epochs
 
-        self.x = np.load(self.path_features)
-        self.y = np.load(self.path_targets)
+        # self.x = np.load(self.path_features)
+        # self.y = np.load(self.path_targets)
 
         return
 
-    def initiate(self, paths, path_features, path_targets):
+    def initiate(self, path, path_features, path_targets):
         """Initiates the project with starting folder and dataset.
 
         :param path:
@@ -59,11 +73,23 @@ class Generator:
             path = Path(path)
 
         # Create directories needed for generation 0
-        path.mkdir(path / 'gen0' / 'ml', parents=True)
-        path.mkdir(path / 'data')
+        try:
+            (path / 'gen0' / 'ml').mkdir(parents=True)
+            (path / 'gen0' / 'data').mkdir()
+        except:
+            i = 0
+            while True:
+                try:
+                    p = Path(str(path) + f'_{i}')
+                    (p / 'gen0' / 'ml').mkdir(parents=True)
+                    (p / 'gen0' / 'data').mkdir()
+                    path = p
+                    break
+                except:
+                    i += 1
 
-        shutil.copy(path_features, path / 'data' / 'features.npy')
-        shutil.copy(path_targets, path / 'data' / 'targets.npy')
+        shutil.copy(path_features, path / 'gen0' / 'data' / 'features.npy')
+        shutil.copy(path_targets, path / 'gen0' / 'data' / 'targets.npy')
 
         # Set current working directory. Updated as we increase the number of generations
         self.proj_direc = path / 'gen0'
@@ -83,9 +109,10 @@ class Generator:
         """
         with open(path, 'w') as f:
             f.write('#!/bin/bash\n')
-            for key, val in arguments:
+            for key, val in arguments.items():
                 f.write(f'#SBATCH --{key}={val}\n')
-            f.write('\n')
+            f.write('\n\n')
+            f.write('echo $CUDA_VISIBLE_DEVICES\n')
             f.write(exec_cmd)
 
     @staticmethod
@@ -98,9 +125,10 @@ class Generator:
         args = {'partition': 'normal',
                 'ntasks': 1,
                 'gres': f'gpu:{N}',
-                'cpus-per-task': 2}
+                'cpus-per-task': 2,
+                'output': 'slurm.out'}
         if kwargs:
-            for key, val in kwargs:
+            for key, val in kwargs.items():
                 args[key] = val
 
         return args
@@ -116,54 +144,55 @@ class Generator:
         """
         args = {'partition': 'normal',
                 'ntasks': n_tasks,
-                'nodes': n_nodes}
+                'nodes': n_nodes,
+                'output': 'slurm.out'}
 
         if kwargs:
-            for key, val in kwargs:
+            for key, val in kwargs.items():
                 args[key] = val
 
         return args
 
     @staticmethod
-    def generate_trainer(path, optimizer=None, criterion=None, params=None):
+    def generate_trainer(path, optimizer, criterion, params, epochs):
         """Generates a python script for training CNN
         """
 
         if optimizer is None:
             optimizer = 'torch.optim.Adam'
         if criterion is None:
-            criterion = torch.nn.MSELoss()
+            criterion = 'torch.nn.MSELoss()'
         if params is None:
             params = {'lr': 1e-4,
                       'weight_decay': 0.01}
 
-        with open(path / 'run_cnn.py', 'w') as f:
+        with open(path / 'ml' / 'run_cnn.py', 'w') as f:
             f.write('import torch \n')
             f.write('from run_torch_model import create_dataloader, RunTorchCNN \n')
             f.write('from cnn import Model \n')
-            f.write('import numpy as np \n')
-            f.write('torch.manual_seed(42) \n')
-            f.write('model = Model() \n')
-            f.write(f'features = np.load({path / "features.npy"}) \n')
+            f.write('import numpy as np \n\n')
+            f.write('torch.manual_seed(42) \n\n')
+            f.write('model = Model() \n\n')
+            f.write(
+                f'features = np.load("{path / "data" / "features.npy"}") \n')
             f.write('features = features[:, np.newaxis, :, :] \n')
-            f.write(f'targets = np.load({path / "targets.npy"}) \n')
-            f.write(f'targets = targets[:, np.newaxis] \n')
-            f.write(f'optimizer = {optimizer} \n')
+            f.write(
+                f'targets = np.load("{path / "data" / "targets.npy"}") \n')
+            f.write(f'targets = targets[:, np.newaxis] \n\n')
+            f.write(f'optimizer = "{optimizer}" \n')
             f.write(f'criterion = {criterion} \n')
-            f.write(f'params = {params} \n')
-            f.write('torch.backends.cudnn.benchmark = True \n')
-            f.write('dataloader_train, dataloader_test=create_dataloader(features=features, targets=targets, batch_size=128, train_size=0.8, test_size=0.2, shuffle=True) \n')
-            f.write('train_cnn = RunTorchCNN(model, epochs=5000, optimizer=optimizer, optimizer_args=params, dataloaders=(dataloader_train, dataloader_test), criterion=criterion, verbose=False, seed=42) \n')
-            f.write('train_cnn() \n')
-            f.write(f'train_cnn.save_model(str({path / "model.pt"})) \n')
-            f.write(f'train_cnn.save_running_metrics(str({path})) \n')
+            f.write(f'params = {params} \n\n')
+            f.write('torch.backends.cudnn.benchmark = True \n\n')
+            f.write('dataloader_train, dataloader_test=create_dataloader(features=features, targets=targets, batch_size=128, train_size=0.8, test_size=0.2, shuffle=True) \n\n')
+            f.write(
+                f'train_cnn = RunTorchCNN(model, epochs={epochs}, optimizer=optimizer, optimizer_args=params, dataloaders=(dataloader_train, dataloader_test), criterion=criterion, verbose=False, seed=42) \n\n')
+            f.write('train_cnn() \n\n')
+            f.write(f'train_cnn.save_model("{path / "ml" / "model.pt"}") \n')
+            f.write(f'train_cnn.save_running_metrics("{path / "ml"}") \n')
 
     def train_cnn(self, file='run_cnn.py',
                   jobscript='job.sh',
                   method='slurm_gpu',
-                  criterion=None,
-                  params=None,
-                  optimizer=None,
                   ):
         """Trains a CNN on data which has been measured by MD simulations of
         material structures. Queues a CNN to start when all MD simulations are
@@ -183,85 +212,140 @@ class Generator:
         # Changing dir to ML
         os.chdir(self.proj_direc / 'ml')
 
+        shutil.copy(self.path_cnn, Path.cwd() / 'cnn.py')
+
         if method == 'slurm_gpu':
             args = self.slurm_gpu(N=1)
         else:
             raise NotImplementedError('Method only supports training with GPU')
 
+        args['job-name'] = 'cnn'
+
         # Generate script to run pytorch NN
-        self.generate_trainer(Path.cwd(), optimizer, criterion, params)
+        self.generate_trainer(self.proj_direc, self.optimizer,
+                              self.criterion, self.params, self.epochs)
         # Generate jobscript and store in .../ml
         self.generate_jobscript(args,
                                 'python3 run_cnn.py',
-                                Path.cwd())
+                                Path.cwd() / 'job.sh')
 
         if self.data_collect_job_id is not None:
             dependency = f'--dependency=afterok:{self.data_collect_job_id}'
         else:
-            dependency = ''
+            dependency = None
 
-        subprocess.check_call(
-            ['ssh', 'bigfacet;', 'sbatch', f'{dependency}', '--wait', 'job.sh'])
+        sshProcess = subprocess.Popen(['ssh', '-tt', 'bigfacet'],  # , ';', f"cd {self.proj_direc}/ml", ';',  f"sbatch job.sh", ';', "logout"],
+                                      stdin=subprocess.PIPE,
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE,
+                                      universal_newlines=True,
+                                      bufsize=0)  # as sshProcess:
 
-        return
+        # out, err = sshProcess.communicate(f"""
+        #     cd {self.proj_direc}/ml
+        #     sbatch {dependency} --wait job.sh
+        # """)
+        # print(out, err)
+        sshProcess.stdin.write(f"cd {self.proj_direc}/ml\n")
+        sshProcess.stdin.write("ls\n")
+        # if dependency is None:
+        sshProcess.stdin.write(f"sbatch job.sh\n")
+        # else:
+        #     sshProcess.stdin.write(f"sbatch {dependency} job.sh\n")
+        sshProcess.stdin.write("logout\n")
+        # result = sshProcess.stdout.readlines()
+        # if result == []:
+        #     error = sshProcess.stderr.readlines()
+        #     print(sys.stderr, "ERROR: %s" % error)
+        # else:
+        #     print(result)
+        sshProcess.stdin.close()
 
-    def create_new_samples(self, parameters):
-        """Create new samples to perform predictions on.
-        """
-        octaves = parameters['octaves']
-
-    def prediction_new_samples(self):
-        """Performs predictions on new samples. Runs on GPU
-        """
-
-        return
-
-    def choose_samples(self):
-        """Choose 100 strongest and 100 weakest.
-        """
-
-        return
-
-    def execute_lammps_files(self, lmp_args, slurm_args, var, N):
-        self.job_ids = np.zeros(N)
-
-        for i in range(N):
-            slurm_args['job_name'] = slurm_args['job_name'] + f'{i}'
-            var['datafile'] = f'{i}.data'
-
-            computer = SlurmGPU(lmp_exec='lmp',
-                                slurm_args=slurm_args,
-                                lmp_args=lmp_args,
-                                job_script='job.sh')
-            sim = Simulator(directory=self.path_data / 'run' / f'{i}')
-            sim.copy_to_wd(self.path_data / f'{i}.data', self.path_potential)
-            sim.set_input_script(self.path_lmp, **var)
-            self.job_ids[i] = sim.run(computer=computer)
+        for line in sshProcess.stdout:
+            if line == "END\n":
+                break
+            print(line, end="")
 
         return
 
-    @staticmethod
-    def create_job_collect_data(path):
-
-    def gather_samples(self, data_paths, output_path):
-        """Gathers samples from different data paths.
+    @ staticmethod
+    def generate_sample_creator(path, parameters):
+        """Creates python file which executes the creation of new samples
         """
-        path_new_features = data_paths['new_features']
-        path_new_targets = data_paths['new_targets']
 
-        jobid_string = ':'.join(map(str, self.job_ids))
-        dependency = f'--dependency=afterok:{jobid_string}'
+        with open(path / 'data' / 'new_samples', 'w') as f:
+            f.write(
+                'from simplexgrid import CreateMultipleSimplexGrids, SeedGenerator\n')
+            f.write('import numpy as np\n')
+            f.write('np.random.seed(42)\n\n')
+            f.write('seedgen = SeedGenerator(start=1, step=1)\n\n')
+            f.write(f'octaves = {parameters["octaves"]}\n')
+            f.write(f'scales = {parameters["scales"]}\n')
+            f.write(f'thresholds = {parameters["thresholds"]}\n')
+            f.write(f'bases = {parameters["bases"]}\n')
+            f.write(f'')
 
-        # Execute program which opens the existing features/targets and appends
-        # the new features/targets, and then creates dataloaders to be used
-        # during training
-        file_collect_data = 'job_collect_data.sh'
-        var = f'{data_path}'
-
-        # str(subprocess.check_output(["sbatch", self.jobscript], stderr=stderr))
-        output = str(subprocess.check_output(
-            ['sbatch', f'{dependency}', f'{file_collect_data}', f'{var}'],
-            stderr=subprocess.PIPE))
-        self.gather_samples_job_id = str(re.findall('([0-9]+)', output)[0])
-
-    def create_direcs(self, gen):
+    # def create_new_samples(self, parameters):
+    #     """Create new samples to perform predictions on.
+    #
+    #     ssh to egil
+    #     sbatch cpu job
+    #         - pass **parameters to simplexgrid
+    #         - creates new samples
+    #     """
+    #
+    # def prediction_new_samples(self):
+    #     """Performs predictions on new samples. Runs on GPU
+    #     """
+    #
+    #     return
+    #
+    # def choose_samples(self):
+    #     """Choose 100 strongest and 100 weakest.
+    #     """
+    #
+    #     return
+    #
+    # def execute_lammps_files(self, lmp_args, slurm_args, var, N):
+    #     self.job_ids = np.zeros(N)
+    #
+    #     for i in range(N):
+    #         slurm_args['job_name'] = slurm_args['job_name'] + f'{i}'
+    #         var['datafile'] = f'{i}.data'
+    #
+    #         computer = SlurmGPU(lmp_exec='lmp',
+    #                             slurm_args=slurm_args,
+    #                             lmp_args=lmp_args,
+    #                             job_script='job.sh')
+    #         sim = Simulator(directory=self.path_data / 'run' / f'{i}')
+    #         sim.copy_to_wd(self.path_data / f'{i}.data', self.path_potential)
+    #         sim.set_input_script(self.path_lmp, **var)
+    #         self.job_ids[i] = sim.run(computer=computer)
+    #
+    #     return
+    #
+    # @staticmethod
+    # def create_job_collect_data(path):
+    #
+    # def gather_samples(self, data_paths, output_path):
+    #     """Gathers samples from different data paths.
+    #     """
+    #     path_new_features = data_paths['new_features']
+    #     path_new_targets = data_paths['new_targets']
+    #
+    #     jobid_string = ':'.join(map(str, self.job_ids))
+    #     dependency = f'--dependency=afterok:{jobid_string}'
+    #
+    #     # Execute program which opens the existing features/targets and appends
+    #     # the new features/targets, and then creates dataloaders to be used
+    #     # during training
+    #     file_collect_data = 'job_collect_data.sh'
+    #     var = f'{data_path}'
+    #
+    #     # str(subprocess.check_output(["sbatch", self.jobscript], stderr=stderr))
+    #     output = str(subprocess.check_output(
+    #         ['sbatch', f'{dependency}', f'{file_collect_data}', f'{var}'],
+    #         stderr=subprocess.PIPE))
+    #     self.gather_samples_job_id = str(re.findall('([0-9]+)', output)[0])
+    #
+    # def create_direcs(self, gen):
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('README.md', 'r') as infile:
+    long_description = infile.read()
+
+setuptools.setup(
+    name='generator',
+    version='0.1.0',
+    author='Christer Dreierstad',
+    author_email='christerdr@outlook.com',
+    description='Generative algorithm for an MD project using CNN to predict strength of materials',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/chdre/generator',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=['torch',
+                      'lammps_simulator @ git+https://git@github.com/evenmn/lammps-simulator@master#egg=lammps_simulator',
+                      'run_torch_model @ git+https://git@github.com/chdre/run-torch-model@master#egg=run_torch_model',
+                      'simplexgrid @ git+https://git@github.com/chdre/simplexgrid@master#egg=simplexgrid',
+                      'data_analyzer @ git+https://git@github.com/chdre/data-analyzer@master#egg=data_analyzer'],
+    include_package_data=True,
+)
