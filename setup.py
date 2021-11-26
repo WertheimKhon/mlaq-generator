1accca0de9858b53fceb7c87e264135e8e4768b1
chrdre <christerdr@outlook.com>
2021-11-26 16:46:19 +0100

    Set training to device and fixed a bug with missing predictions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as infile:
 
 setuptools.setup(
     name='generator',
-    version='0.1.1',
+    version='0.1.2',
     author='Christer Dreierstad',
     author_email='christerdr@outlook.com',
     description='Generative algorithm for an MD project using CNN to predict strength of materials',
